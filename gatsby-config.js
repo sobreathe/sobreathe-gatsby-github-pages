4fccc6969785c8a2a7bc0ac7b717300cfdd6273b
Jonas Lit <melynasdangus@gmail.com>
2022-09-04 09:59:46 +0300

    Initial commit 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/sobreathe-gatsby-github-pages",
   siteMetadata: {
     title: `So Breathe`,
     siteUrl: `https://www.sobreathe.com`,
---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const PrivacyPolicy = () => {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default PrivacyPolicy
